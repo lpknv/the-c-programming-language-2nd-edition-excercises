740bd9d1662caeda0eb811c8902b4e3199b56241
lpknv <nikitalipkanov@outlook.com>
2025-10-15 22:09:56 +0200

    chore

--- ch-1.2/main.c
+++ ch-1.2/main.c
@@ -16,7 +16,7 @@ int main() {
   printf("Fahrenheit\tCelsius\n");
   while (fahr <= UPPER) {
     celsius = 5.0 * (fahr - 32) / 9.0;
-    printf("%.f\t%.1f\n", fahr, celsius);
+    printf("%.f\t\t%.1f\n", fahr, celsius);
     fahr += STEP;
   }
 }
