f506320acf9e27c5bb6109a4ab45b8653a01fccb
lpknv <nikitalipkanov@outlook.com>
2025-10-15 22:12:35 +0200

    stuff

--- ch-1.2/main.c
+++ ch-1.2/main.c
@@ -13,10 +13,20 @@ int main() {
   float celsius = 0;
   float fahr = 0;
 
-  printf("Fahrenheit\tCelsius\n");
+  printf("\nFahrenheit\tCelsius (lower to upper)\n");
   while (fahr <= UPPER) {
     celsius = 5.0 * (fahr - 32) / 9.0;
     printf("%.f\t\t%.1f\n", fahr, celsius);
     fahr += STEP;
+  }
+
+  celsius = 0;
+  fahr = UPPER;
+
+  printf("\n\nFahrenheit\tCelsius (upper to lower)\n");
+  while (fahr >= LOWER) {
+    celsius = 5.0 * (fahr - 32) / 9.0;
+    printf("%.f\t\t%.1f\n", fahr, celsius);
+    fahr -= STEP;
   }
 }
