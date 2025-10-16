710bf4694a1f82fe20af3d3c1c083e468b553b90
lpknv <nikitalipkanov@outlook.com>
2025-10-16 10:13:15 +0200

    chore

--- ch-1.9/main.c
+++ ch-1.9/main.c
@@ -6,44 +6,45 @@
 #include <stdio.h>
 #define MAXLINE 1000
 
-int _getline(char s[], int lim) {
-  int c, i;
+int getlinelength(char mystring[], int limit) {
+  int character, i;
 
-  for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
-    s[i] = c;
-  if (c == '\n') {
-    s[i] = c;
+  for (i = 0;
+       i < limit - 1 && (character = getchar()) != EOF && character != '\n';
+       ++i)
+    mystring[i] = character;
+  if (character == '\n') {
+    mystring[i] = character;
     ++i;
   }
 
-  s[i] = '\0';
+  mystring[i] = '\0';
   return i;
 }
 
 /* copy: copy 'from' into 'to'; assume to is big enough */
 void copy(char from[], char to[]) {
-  int i;
-  i = 0;
+  int i = 0;
   while ((to[i] = from[i]) != '\0')
     ++i;
 }
 
 int main() {
 
-  int len;               /* current line length */
-  int max;               /* maximum length seen so far */
-  char line[MAXLINE];    /* current input line */
-  char longest[MAXLINE]; /* longest line saved here */
+  int length;                /* current line length */
+  int max;                   /* maximum length seen so far */
+  char line[MAXLINE];        /* current input line */
+  char longestLine[MAXLINE]; /* longest line saved here */
 
   max = 0;
 
-  while ((len = _getline(line, MAXLINE)) > 0)
-    if (len > max) {
-      max = len;
-      copy(line, longest);
+  while ((length = getlinelength(line, MAXLINE)) > 0)
+    if (length > max) {
+      max = length;
+      copy(line, longestLine);
     }
 
   if (max > 0) /* there was a line */
-    printf("%s", longest);
+    printf("%s", longestLine);
   return 0;
 }
