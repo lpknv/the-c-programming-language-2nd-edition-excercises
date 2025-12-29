2c45ab389b371b889215eb7225940467192dba3a
lpknv <nikitalipkanov@outlook.com>
2025-12-29 01:01:38 +0100

    add chapter 4.7 Functions

---
+++ ch-4.7/main.c
@@ -0,0 +1,27 @@
+/*
+  Chapter 4.7
+  Functions
+*/
+
+#include <stdio.h>
+
+int power(int m, int n);
+
+int main() {
+  int i = 0;
+
+  for (i = 0; i < 10; ++i)
+    printf("%d %d %d\n", i, power(2, i), power(-3, i));
+  return 0;
+}
+
+int power(int base, int n) {
+  int i, p;
+
+  p = 1;
+
+  for (i = 1; i <= n; ++i)
+    p = p * base;
+
+  return p;
+}
